35c705435f5fce28f2fc5dad86a14d99c684dc11
mihdan15 <mihdan40@gmail.com>
2025-08-16 10:57:11 +0700

    adding cors

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://ketik-matic.mihdanadvani.my.id/",
+      "https://ketik-matic.mihdanadvani.my.id",
     ],
     credentials: true,
   })
